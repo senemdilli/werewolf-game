5b3adb29013cfcbc2451ab5a4dd70f0dff8179ec
txmbau <baumann.tim@protonmail.com>
2026-06-09 08:57:09 +0200

    feat: add dummy implementation of game client

---
+++ agents/client.py
@@ -0,0 +1,6 @@
+"""Game client for interacting with the game server."""
+
+class Client:
+    async def send_message(self, message):
+        """Dummy implementation"""
+        print(f"Sending message: {message}")
